1df19fc3f938913897dbc5228390e580c3670c76
sc <924340328@qq.com>
2021-01-04 15:25:27 +0800

    idea首次第二次修改测试git提交

--- src/main/java/com/sc/MyClass.java
+++ src/main/java/com/sc/MyClass.java
@@ -3,5 +3,8 @@ package com.sc;
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+
     }
 }
